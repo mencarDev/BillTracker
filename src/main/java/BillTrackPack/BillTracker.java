176c5e747d59bfccfd4ae683c44b0b8eaad7d1a2
Marvin Encarnacion <marvinenc37@gmail.com>
2026-03-16 13:33:19 -0400

    Added a print statement

---
+++ src/main/java/BillTrackPack/BillTracker.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package BillTrackPack;
+
+/**
+ *
+ * @author Marvin
+ */
+
+public class BillTracker 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
