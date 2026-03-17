5bc454e20efc9973ff5b1170d6e95b62cd063c15
Marvin Encarnacion <marvinenc37@gmail.com>
2026-03-17 00:40:16 -0400

    Created a program that takes user input and generates a URL from the input, later returning the contents of the generated URL

--- src/main/java/BillTrackPack/BillTracker.java
+++ src/main/java/BillTrackPack/BillTracker.java
@@ -8,26 +8,68 @@ package BillTrackPack;
  *
  * @author Marvin
  */
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
-
-
-class HTMLpull
-{
-    static String baseURL = "https://m.flsenate.gov/Bill/";
-
-    public static void elementRetrieve(String bn) 
-    {
-        String billYear = bn.concat("/2026");
-        String billURL = baseURL.concat(billYear);
-    }
-}
 
 public class BillTracker 
 {
+    static String baseURL = "https://m.flsenate.gov/Bill/"; //A string that represents the base URL for bill search
+    static String billNum; //A String that will be assigned the bill number
+    static String billURL; //A string that will represent the URL for the desired bill
+
+    //A method that concatenates the bill number with the bill year and concatenates the result to the baseURL to create a string for the URL
+    public static void URLGenerate(String bn) 
+    {
+        String billYear = bn.concat("/2026");
+        billURL = baseURL.concat(billYear);
+    }
+
+    //A method that turns the URL string into a URL object and opens a connection to the URL to extract the contents of the underlying HTML, building a String of those contents
+    public static String getHTML(String urlString) throws IOException
+    {
+        StringBuilder htmlContent = new StringBuilder();
+        URL url = new URL(billURL);
+        URLConnection connection = url.openConnection();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream())))
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                htmlContent.append(line);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("An unexpected error has ocurred");
+        }
+
+        return htmlContent.toString();
+
+    }
+
 
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the desired bill number");
+        
+        billNum = input.next();
+        URLGenerate(billNum);
+        
+        try
+        {
+            String html = getHTML(billURL);
+            System.out.println(html);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
